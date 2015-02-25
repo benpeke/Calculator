b590b5b58f6e028f2e57719e0bfcedc05cbe6ab6
Benjamin Schwartz <benpeke@yahoo.com>
2015-02-24 22:02:38 -0800

    second commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var historyDisplay: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    var brain = CalculatorBrain()
+
+    var displayValue:Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTyping = false
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        historyDisplay.text = historyDisplay.text! + digit
+        if (digit != "." || display.text!.rangeOfString(".") != nil) {
+            if (userIsInTheMiddleOfTyping) {
+                display.text = display.text! + digit
+            } else {
+                display.text = digit
+                userIsInTheMiddleOfTyping = true
+            }
+        }
     }
 
+    @IBAction func clear(sender: UIButton) {
+        brain.clear()
+        display.text = "0"
+    }
 
+    @IBAction func enter() {
+        userIsInTheMiddleOfTyping = false
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
+    }
+
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        historyDisplay.text = historyDisplay.text! + operation
+        if (userIsInTheMiddleOfTyping) {
+            enter()
+        }
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
+    }
 }
-
